f564730befdcc0d860d5e49b2d543957794478e2
Taeim Kwon <tkwon@unc.edu>
2022-12-04 09:46:31 -0500

    Modified beauty-histogram.R

--- beauty-histogram.R
+++ beauty-histogram.R
@@ -1,7 +1,12 @@
 library(tidyverse)
+library(patchwork)
 
 linkedin_derived <- read_csv("derived_data/linkedin_derived.csv")
 
-beauty <- ggplot(linkedin_derived, aes(beauty)) + geom_histogram()
+beauty <- ggplot(linkedin_derived, aes(beauty)) + geom_histogram() + xlab("Beauty")
+beauty_male <- ggplot(linkedin_derived, aes(beauty_male)) + geom_histogram() + xlab("Beauty Male")
+beauty_feamle <- ggplot(linkedin_derived, aes(beauty_female)) + geom_histogram() + xlab("Beauty Female")
 
-ggsave("figures/beauty_histogram.png", beauty)
+png("figures/beauty_histogram.png")
+beauty + beauty_feamle + beauty_male
+dev.off()
