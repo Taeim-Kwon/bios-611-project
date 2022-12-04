a01019489279faf2d7180fbe77af86d03a1176f1
Taeim Kwon <tkwon@unc.edu>
2022-12-04 09:47:48 -0500

    Modified scatter-plot.R

--- scatter-plot.R
+++ scatter-plot.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-linkedin_derived <- read_csv("derived_data/linkedin_derived.csv")
+linkedin_derived <- read_csv("derived_data/linkedin_derived.csv") %>% select(-1)
 
 age_beauty <- ggplot(linkedin_derived, aes(age, beauty)) + geom_point()
 
