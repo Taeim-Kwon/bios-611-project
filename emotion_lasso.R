7e1d17358704709199e8c166e24ad95561d1b52b
Taeim Kwon <tkwon@unc.edu>
2022-12-04 09:39:19 -0500

    Modified emotion_lasso.R

--- emotion_lasso.R
+++ emotion_lasso.R
@@ -15,7 +15,7 @@ x <- model.matrix(male~emo_anger+emo_disgust+emo_fear+emo_happiness+emo_neutral+
 y <- train.lasso$male
 glmnet(x, y, family = "binomial", alpha = 1, lamda = NULL)
 
-# cross validation for optimal lambda
+# Cross validation for optimal lambda
 cv.lasso <- cv.glmnet(x, y, alpha = 1, family = "binomial")
 coef(cv.lasso, cv.lasso$lambda.min)
 
@@ -23,21 +23,21 @@ png("figures/emo_lasso.png")
 plot(cv.lasso)
 dev.off()
 
-# lasso model with lambda.min
+# Lasso model with lambda.min
 model.lambda.min <- glmnet(x, y, alpha = 1, family = "binomial", lamda = cv.lasso$lambda.1se)
 
-# accuracy of model with lambda.min
+# Accuracy of model with lambda.min
 x.test <- model.matrix(male~emo_anger+emo_disgust+emo_fear+emo_happiness+emo_neutral+emo_sadness+emo_surprise, test.lasso)[,-1]
 probabilities <- model.lambda.min %>% predict(newx = x.test)
 predicted <- ifelse(probabilities > 0.5, 1, 0)
 observed <- test.lasso$male
 mean(predicted == observed)
 
-# full logistic model
+# Logistic model
 log.model <- glm(male~emo_anger+emo_disgust+emo_fear+emo_happiness+emo_neutral+emo_sadness+emo_surprise, train.lasso, family = "binomial")
 summary(log.model)
 
-# accuracy of full logistic model
+# Accuracy of full logistic model
 probabilities <- log.model %>% predict(test.lasso, type = "response")
 predicted <- ifelse(probabilities > 0.5, 1, 0)
 observed <- test.lasso$male
