8e2829a1c33694d5a177fb2196e3b7d32a97a549
Taeim Kwon <tkwon@unc.edu>
2022-12-04 09:47:08 -0500

    Modified log-density.R

--- log-density.R
+++ log-density.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-linkedin_derived <- read_csv("derived_data/linkedin_derived.csv")
+linkedin_derived <- read_csv("derived_data/linkedin_derived.csv") %>% select(-1)
 
 avg_prev_length <- ggplot(linkedin_derived, aes(log(avg_previous_position_length))) + geom_density()
 
