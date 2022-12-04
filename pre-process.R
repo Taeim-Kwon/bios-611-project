cba2d3f2c4f2cec139072292685d3984e7bf3450
Taeim Kwon <tkwon@unc.edu>
2022-12-04 09:47:27 -0500

    Modified pre-process.R

--- pre-process.R
+++ pre-process.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 
+# linkedin_derived
 linkedin_derived <- read_csv("source_data/LinkedIn_Profile_Data.csv") %>% 
   rename(emo_disgust = emp_disgust) %>% distinct()
+
+linkedin_derived <- linkedin_derived %>% select(-1) %>% unique()
 
 linkedin_derived %>% write.csv("derived_data/linkedin_derived.csv")
